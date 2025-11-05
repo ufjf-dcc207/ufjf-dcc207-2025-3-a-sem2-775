1c629227840e3d5d7cac8da17ffea1bdaa49613c
Guilherme Tavares <tavares.guilherme@estudante.ufjf.br>
2025-11-05 18:28:49 -0300

    arruma arquivos json

--- Le-petit-café/vite.config.ts
+++ Le-petit-café/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ufjf-dcc207-2025-3-a-trabalho1-guilhermetavares",
   plugins: [react()],
 })
