0925fde07b67a7d1afeef8582a6e443b74d349a1
Guilherme Tavares <tavares.guilherme@estudante.ufjf.br>
2025-11-05 23:34:18 -0300

    cria elemento da primeira opção do menu

--- Le-petit-café/src/App.tsx
+++ Le-petit-café/src/App.tsx
@@ -1,10 +1,11 @@
 
 import './App.css'
+import Opcao1 from "./opcao1";
 
 function App() {
-  return <div className="app">Olá mundo!  </div>
+  return <div className="app"><Opcao1/> </div>
     //teste
   
 }
 
-export default App
+export default App;
---
+++ Le-petit-café/src/opcao1.tsx
@@ -0,0 +1,5 @@
+import "./opcao1.css";
+
+export default function Opcao1() {
+  return <div className="opcao1">Aurore Component</div>;
+}
