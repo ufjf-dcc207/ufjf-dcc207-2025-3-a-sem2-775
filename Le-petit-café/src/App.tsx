c93a460878859c0297de01414d9d9f0dc4395467
Guilherme Tavares <tavares.guilherme@estudante.ufjf.br>
2025-11-06 01:00:51 -0300

    ajustat elementos e estilização

--- Le-petit-café/src/App.tsx
+++ Le-petit-café/src/App.tsx
@@ -3,8 +3,8 @@ import './App.css'
 import Opcao1 from "./opcao1";
 
 function App() {
-  return <div className="app"><Opcao1/> </div>
-    //teste
+  return <div className="app"><Opcao1 /> </div>
+    
   
 }
 
--- Le-petit-café/src/opcao1.tsx
+++ Le-petit-café/src/opcao1.tsx
@@ -1,5 +1,25 @@
 import "./opcao1.css";
 
-export default function Opcao1() {
-  return <div className="opcao1">Aurore Component</div>;
+interface opcao1Props {
+  icone?: string;
+  nome?: string;
+  ingredientes?: string[];
+  preco?: string;
+}
+
+export default function Opcao1({
+   icone="🍞",
+   nome = "Aurore Parisienne",
+   ingredientes = ["Farinha de trigo", "Água", "Fermento", "Sal"],
+   preco = "R$ 5,00",
+  }: opcao1Props) {
+  return (
+   <div className="opcao1">
+      <div>{icone}</div>
+      <div>{nome}</div>
+      <div>{ingredientes.join(", ")}</div>
+      <div>{preco}</div>
+      </div>
+
+  );
 }
